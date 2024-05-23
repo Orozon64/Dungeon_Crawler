12b740afcd957c6b5ebd266a483e34d84dc41d44
Orozon64 <128033558+Orozon64@users.noreply.github.com>
2024-05-23 09:35:03 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,39 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class Main {
+    static ArrayList<RoomCell> cells = new ArrayList<>();
+    static int Dijkstra(RoomCell start, RoomCell finish){
+        ArrayList <RoomCell> Q = cells;
+        for(RoomCell inf_setter : Q){
+            if(inf_setter == start){
+                inf_setter.dis = 0;
+            }
+            else{
+                inf_setter.dis = Double.POSITIVE_INFINITY;
+            }
+
+        }
+        while (!Q.isEmpty()){
+            RoomCell v = new RoomCell('X', -99, -99);
+            int distance = 9999;
+            for(RoomCell ge: Q){ //szukamy wierzchołka o najmniejszej odległości
+                if(ge.dis < distance){
+                    distance = (int) ge.dis;
+                    v = ge;
+                }
+            }
+            Q.remove(v);
+            for(RoomEdge k : v.edges){
+                RoomCell u = k.cell_to_connect;
+                if(k.weight < u.dis){
+                    u.dis = v.dis + k.weight;
+                    u.previous = v;
+                }
+            }
+        }
+        return (int) finish.dis;
+    }
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         Random rand = new Random();
@@ -15,11 +48,12 @@ public class Main {
         char content;
         int exit_x;
         int exit_y;
-        ArrayList<RoomCell> cells = new ArrayList<>();
+
         ArrayList<Integer> dangers_x = new ArrayList<>();
         ArrayList<Integer> dangers_y = new ArrayList<>();
         ArrayList<Integer> treasures_x = new ArrayList<>();
         ArrayList<Integer> treasures_y = new ArrayList<>();
+        int cellnum = 0;
         System.out.println("Legenda: P = puste pole, Z = zagrożenie(pułapka, potwór), S=skarb, W=wyjście, G=gracz");
         for(int n = 0; n < num_of_rooms; n++){
             room_width = rand.nextInt(8) + 2;
@@ -56,15 +90,15 @@ public class Main {
                 for(int w = 0; w < room_width; w++){
 
                     if(num_of_dangers != 0 && dangers_x.contains(w) && dangers_y.contains(h)){
-                            content = 'Z';
-                            num_of_dangers--;
-                            System.out.print(content);
+                        content = 'Z';
+                        num_of_dangers--;
+                        System.out.print(content);
                     }
                     else if(num_of_treasures!= 0 && treasures_x.contains(w) && treasures_y.contains(h)){
 
-                            content = 'S';
-                            num_of_treasures--;
-                            System.out.print(content);
+                        content = 'S';
+                        num_of_treasures--;
+                        System.out.print(content);
 
                     }
                     else if (w == exit_x && h == exit_y) {
@@ -81,18 +115,56 @@ public class Main {
                     }
                     RoomCell rc = new RoomCell(content, w+1, h+1);
                     cells.add(rc);
+                    if(w != 0 || h!= 0){ //jeśli nie jesteśmy na pierwszej komórce
+                        if (w != 0){
+                            RoomCell connection_cell = cells.get(cellnum-1);
+                            int con_w;
+                            if(connection_cell.content == 'Z'){
+                                con_w = 99;
+                            }
+                            else {
+                                con_w = 1;
+                            }
+                            RoomEdge re = new RoomEdge(connection_cell, con_w);
+                            rc.edges.add(re);
+                        }
+                        if(h!= 0){
+                            RoomCell connection_cell = cells.get(cellnum-room_width);
+                            int con_w;
+                            if(connection_cell.content == 'Z'){
+                                con_w = 99;
+                            }
+                            else {
+                                con_w = 1;
+                            }
+                            RoomEdge re = new RoomEdge(connection_cell, con_w);
+                            rc.edges.add(re);
+                        }
+
+                    }
                 }
                 System.out.println("");
+                cellnum++;
             }
             System.out.println("Wskaż koordynat x swojego celu");
             user_xcoord = Integer.parseInt(scn.nextLine());
             System.out.println("Wskaż koordynat y swojego celu");
             user_ycoord = Integer.parseInt(scn.nextLine());
             //wejście danych od użytkownika
+            RoomCell destination;
+            RoomCell player_start;
+            for(RoomCell cell: cells){
+                if(cell.xcoord == user_xcoord && cell.ycoord == user_ycoord){
+                    destination = cell;
+                }
+            }
+
+            Dijkstra(player_start, destination);
             dangers_x.clear();
             dangers_y.clear();
             treasures_x.clear();
             treasures_y.clear();
+
         }
 
     }
